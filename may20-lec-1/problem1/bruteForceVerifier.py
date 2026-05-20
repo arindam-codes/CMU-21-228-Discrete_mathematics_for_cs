a6477015c5bfab9a4f66692566bb9f8600857140
Arindam B <arindamcodes394@gmail.com>
2026-05-20 17:49:43 +0530

    atleast in workable condition

--- may20-lec-1/problem1/bruteForceVerifier.py
+++ may20-lec-1/problem1/bruteForceVerifier.py
@@ -3,12 +3,16 @@ def count_all_even_brute():
     even = 0
 
     while n <= 9999:
-        if n % 2 == 0:
+        if  (n % 10) % 2 == 0 and ((n % 100) // 10) % 2 == 0 and ((n % 1000) // 100) % 2 == 0 and ((n % 10000) // 1000) % 2 == 0:
             even += 1
-            
+        
         n += 1
 
     return even
 
 
 print(count_all_even_brute())
+
+## this testsall the whole digit but my question asked for every even digits ahh so now i used floor division and 
+## module operator to test individual digits 
+
